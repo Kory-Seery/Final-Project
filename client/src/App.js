db62e549538d9de7857b11fff27a11d12a2fa9af
Kory Seery <kory.seery23@gmail.com>
2024-01-15 13:35:29 -0500

    Ks - Homepage and create dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 //import {useEffect} from "react"
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-
 import Homepage from "./Homepage"
+import Globalstyles from "./Globalstyles";
 
 const App = () => {
-
-
   return (
   <BrowserRouter>
+  <Globalstyles/>
     <Routes>
         <Route path="/" element={<Homepage/>}/>
     </Routes>
